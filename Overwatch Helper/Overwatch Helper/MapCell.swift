338e30340d3f394d0b9ed049634fef9f2a7d2afa
bygong <gongbenyan@gmail.com>
2017-03-08 15:09:26 -0600

    map intro draft

--- Overwatch Helper/Overwatch Helper/MapCell.swift
+++ Overwatch Helper/Overwatch Helper/MapCell.swift
@@ -10,12 +10,32 @@ import UIKit
 
 class MapCell: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var img: UIImageView
+    var lbl: UILabel
+    
+    
+    init(frame: CGRect, image: UIImage, label: String, _ left: Bool) {
+        img = UIImageView(frame: CGRect(x: left ? 50 : 0, y: 0, width: frame.width - 50, height: frame.height))
+        img.image = image
+        
+        lbl = UILabel(frame: CGRect(x: left ? 50 : 0, y: 30, width: frame.width - 50, height: 50))
+        lbl.textColor = UIColor.white
+        lbl.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2)
+        lbl.font = UIFont(name: "Verdana-BoldItalic", size: 15)
+        lbl.text = label
+        lbl.textAlignment = .center
+        
+        super.init(frame: frame)
+        
+        self.addSubview(img)
+        self.addSubview(lbl)
+        
+        self.layer.masksToBounds = true
+        self.layer.cornerRadius = 10
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
 }
--- Overwatch Helper/Overwatch Helper/MapIntroController.swift
+++ Overwatch Helper/Overwatch Helper/MapIntroController.swift
@@ -10,9 +10,27 @@ import Foundation
 import UIKit
 
 class MapIntroController: UIViewController{
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    var mapList : [MapCell] = []
+    var mapNameList : [String] = ["map_volskayaindustry","map_nepal","map_antarctica","map_dorado","map_templeofanubis","map_oasis","map_numbani","map_route66","map_gibraltar","map_hanamura","map_hollywood","map_eichenwalde","map_lijiangtower","map_Ilios"]
+    var mapNames : [String] = ["VOLSKAYA INDUSTRY","NEPAL","ANTARCTICA","DORADO","TEMPLE OF ANUBIS","OASIS","NUMBANI","ROUTE 66","GIBRALTAR","HANAMURA","HOLLYWOOD","EICHENWALDE","LIJIANG TOWER","ILIOS"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        self.scrollView.delegate = self
+        
+        scrollView.contentSize = CGSize(width: 375, height: 750)
+        for i in 0...6 {
+            
+            let width1 = i % 2 == 0 ? 275 : 200
+            let width2 = 475 - width1
+            let newMap1 = MapCell(frame: CGRect(x: -50, y: -50 + i*120, width: width1, height: 100), image: UIImage(named: mapNameList[2*i])!, label: mapNames[2*i], true)
+            self.scrollView.addSubview(newMap1)
+            
+            let newMap2 = MapCell(frame: CGRect(x: width1 - 40, y: -50 + i*120, width: width2, height: 100), image: UIImage(named: mapNameList[2*i+1])!, label: mapNames[2*i+1], false)
+            self.scrollView.addSubview(newMap2)
+        }
     }
     
     override func didReceiveMemoryWarning() {
