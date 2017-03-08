cdd8a76b9bbcc04ac3e0158d335b6ac50b0ba7f7
bygong <gongbenyan@gmail.com>
2017-03-08 14:57:04 -0600

    map intro draft

---
+++ Overwatch Helper/Overwatch Helper/MapCell.swift
@@ -0,0 +1,21 @@
+//
+//  MapCell.swift
+//  Overwatch Helper
+//
+//  Created by Benyan Gong on 3/8/17.
+//  Copyright © 2017 Alex&Ben. All rights reserved.
+//
+
+import UIKit
+
+class MapCell: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
