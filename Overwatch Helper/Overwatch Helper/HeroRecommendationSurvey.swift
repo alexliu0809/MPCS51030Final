fbe223fe57a55cdcf7501082cc1bbcc620ec1d2a
bygong <gongbenyan@gmail.com>
2017-03-09 23:24:47 -0600

    11:24PM

--- Overwatch Helper/Overwatch Helper/HeroRecommendationSurvey.swift
+++ Overwatch Helper/Overwatch Helper/HeroRecommendationSurvey.swift
@@ -7,22 +7,73 @@
 //
 
 import Foundation
+import UIKit
 
-class HeroRecommendationSurvey {
+class HeroRecommendationSurvey: UIView {
     var HeroScores: [Int] = []
     var Questions: [RecommendationQuestion] = []
+    var nextQuestionIndex = 0
     
-    @objc func choose1(){
+    init(){
+        super.init(frame: CGRect(x: 0, y: 0, width: 375, height: 500))
+        let question = UILabel(frame: (CGRect(x: 20, y: 100, width: 200, height: 50)))
+        let option1 = UIButton(frame: CGRect(x: 20, y: 160, width: 200, height: 50))
+        let option2 = UIButton(frame: CGRect(x: 20, y: 220, width: 200, height: 50))
+        let option3 = UIButton(frame: CGRect(x: 20, y: 280, width: 200, height: 50))
+        let option4 = UIButton(frame: CGRect(x: 20, y: 340, width: 200, height: 50))
+        question.text = "question1"
+        question.backgroundColor = UIColor.brown
+        
+        option1.titleLabel?.text = "Option1"
+        option1.setTitle("Option1", for: .normal)
+        option1.backgroundColor = UIColor.brown
+        
+        option2.titleLabel?.text = "Option2"
+        option2.setTitle("Option2", for: .normal)
+        
+        option3.titleLabel?.text = "Option3"
+        option3.setTitle("Option3", for: .normal)
+        
+        option4.titleLabel?.text = "Option4"
+        option4.setTitle("Option4", for: .normal)
+
+        option1.addTarget(self, action: #selector(self.choose1), for: .touchUpInside)
+        option2.addTarget(self, action: #selector(self.choose2), for: .touchUpInside)
+        option3.addTarget(self, action: #selector(self.choose3), for: .touchUpInside)
+        option4.addTarget(self, action: #selector(self.choose4), for: .touchUpInside)
+        
+        self.addSubview(question)
+        self.addSubview(option1)
+        self.addSubview(option2)
+        self.addSubview(option3)
+        self.addSubview(option4)
+        
+        nextQuestion()
+    }
+    
+    
+    
+    func choose1(){
         print("choose1")
     }
     func choose2(){
-        
+        print("choose2")
     }
     func choose3(){
         
     }
     func choose4(){
-        
+        print("choose4")
+    }
+    
+    func nextQuestion() {
+        if nextQuestionIndex == Questions.count{
+            return
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
 }
