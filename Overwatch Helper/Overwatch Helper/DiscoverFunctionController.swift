7a9c665aee05b906bd103e77ac160a49cd39fef7
bygong <gongbenyan@gmail.com>
2017-03-10 15:11:47 -0600

    splash screen

--- Overwatch Helper/Overwatch Helper/DiscoverFunctionController.swift
+++ Overwatch Helper/Overwatch Helper/DiscoverFunctionController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 enum functionType{
     case whatsToday
@@ -20,23 +21,15 @@ class DiscoverFunctionController: UIViewController, SurveyDelegate, DrawHeroDele
     var functionType: functionType = .none
     var survey: HeroRecommendationSurvey?
     var draw: DrawRandomHero?
+    var LFG: UITableView?
+    var posts: [(String,String,UIImage)] = []
+    
 //    var result: HeroIntroInfo
     
     override func viewDidLoad() {
         super.viewDidLoad()
       
-        switch functionType {
-        case .whatsToday:
-            self.draw = DrawRandomHero()
-            self.view.addSubview(self.draw!)
-            self.draw?.start()
-        case .heroRecommend:
-            self.survey = HeroRecommendationSurvey()
-            self.view.addSubview(survey!)
-            survey?.surveyStart()
-        default:
-            break
-        }
+        
         // Do any additional setup after loading the view.
     }
     
@@ -60,11 +53,34 @@ class DiscoverFunctionController: UIViewController, SurveyDelegate, DrawHeroDele
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "NewsCell", for: indexPath) as! NewsTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "lfgCell", for: indexPath) as! LFGCell
+//        cell.avatar = posts[indexPath.item]
+        cell.descrip.isScrollEnabled = false
+        let marginGuide = cell.contentView.layoutMarginsGuide
+        
+        cell.avatar.translatesAutoresizingMaskIntoConstraints = false
+        cell.battleID.translatesAutoresizingMaskIntoConstraints = false
+        cell.descrip.translatesAutoresizingMaskIntoConstraints = false
+        
+        cell.avatar.leadingAnchor.constraint(equalTo: marginGuide.leadingAnchor).isActive = true
+        cell.avatar.topAnchor.constraint(equalTo: marginGuide.topAnchor).isActive = true
+        cell.avatar.bottomAnchor.constraint(equalTo: marginGuide.bottomAnchor).isActive = true
+        cell.avatar.heightAnchor.constraint(equalTo: cell.avatar.widthAnchor).isActive = true
+        
+        cell.battleID.topAnchor.constraint(equalTo: marginGuide.topAnchor).isActive = true
+        
+        cell.descrip.leadingAnchor.constraint(equalTo: cell.avatar.trailingAnchor).isActive = true
+        cell.descrip.trailingAnchor.constraint(equalTo: marginGuide.trailingAnchor).isActive = true
+        cell.descrip.topAnchor.constraint(equalTo: cell.battleID.bottomAnchor).isActive = true
+        cell.descrip.bottomAnchor.constraint(equalTo: marginGuide.bottomAnchor).isActive = true
+        
+        cell.avatar.image = posts[indexPath.item].2
+        cell.battleID.text = posts[indexPath.item].0
+        cell.descrip.text = posts[indexPath.item].1
         
         return cell
     }
@@ -80,14 +96,36 @@ class DiscoverFunctionController: UIViewController, SurveyDelegate, DrawHeroDele
     }
     
     override func viewWillAppear(_ animated: Bool) {
-//        switch functionType {
-//        case .whatsToday:
-//            self.whatsToday()
-//        case .heroRecommend:
-//            self.heroRecommend()
-//        case .lookingForGroup:
-//            self.lookingForGroup()
-//        default: break
+        switch functionType {
+        case .whatsToday:
+            self.draw = DrawRandomHero()
+            self.view.addSubview(self.draw!)
+            self.draw?.start()
+        case .heroRecommend:
+            self.survey = HeroRecommendationSurvey()
+            self.view.addSubview(survey!)
+            survey?.surveyStart()
+        case .lookingForGroup:
+            self.LFG = UITableView(frame: CGRect(x: 0, y: 0, width: 375, height: 667))
+            LFG?.delegate = self
+            LFG?.dataSource = self
+            LFG?.register(LFGCell.self, forCellReuseIdentifier: "lfgCell")
+            self.view.addSubview(LFG!)
+            LFG?.estimatedRowHeight = 100
+            LFG?.rowHeight = UITableViewAutomaticDimension
+            LFG?.reloadData()
+            
+            //https://medium.com/@satindersingh71/self-sizing-table-view-cells-programmatically-b0e82a20f264#.watb3p8i1 selfsizing
+            LFG?.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+            LFG?.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+            LFG?.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+            LFG?.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+            
+            posts = fetchLFGData()
+        
+        default:
+            break
+        }
     }
     
 //    override func viewWillDisappear(_ animated: Bool) {
@@ -97,6 +135,27 @@ class DiscoverFunctionController: UIViewController, SurveyDelegate, DrawHeroDele
 ////        }
 //    }
     
+    func fetchLFGData() -> [(String,String,UIImage)] {
+        
+        var ret: [(String,String,UIImage)] = []
+        SharedNetworking.Shared.fetchData(URLString: "", completion: {
+            JsonData in
+            
+            let json = JSON(data: JsonData!)
+            for i in json.array!{
+                var avatar: UIImage = UIImage()
+                
+                SharedNetworking.Shared.fetchImage(URLString: i["avatar"].string!, completion: { img in
+                    avatar = img!
+                })
+                let battleID = i["ID"].string
+                let descrip = i["Content"].string
+                ret.append((battleID!,descrip!,avatar))
+            }
+        })
+        
+        return ret
+    }
         
     
 }
---
+++ Overwatch Helper/Overwatch Helper/LFGMessage.swift
@@ -0,0 +1,16 @@
+//
+//  LFGMessage.swift
+//  Overwatch Helper
+//
+//  Created by Benyan Gong on 3/10/17.
+//  Copyright © 2017 Alex&Ben. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct LFGMessage {
+    var battleID: String
+    var avatar: UIImage
+    var descrip: String
+}
